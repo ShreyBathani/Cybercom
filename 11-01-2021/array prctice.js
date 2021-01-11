172fdcc4b1011d4a946a3628e4c18898fdfd45ee
Shrey Bathani <shreybathani007@gmail.com>
2021-01-11 19:31:34 +0530

    task 11-01-2021

---
+++ 11-01-2021/array prctice.js
@@ -0,0 +1,23 @@
+var fruits = ["Banana", "Watermalon", "Grapes", "Mango"];
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.push("Kiwi");
+console.log(fruits);
+
+
+fruits.shift(); 
+console.log(fruits);
+
+fruits.unshift("Lemon");
+console.log(fruits);
+
+var citrus = fruits.slice(1);
+console.log(fruits);
+
+
+
+
+
