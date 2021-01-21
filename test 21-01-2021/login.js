bac06616b6bdc36d681277a03a6ac1597a8a5f27
Shrey Bathani <shreybathani007@gmail.com>
2021-01-21 12:41:10 +0530

    second commit

--- test 21-01-2021/login.js
+++ test 21-01-2021/login.js
@@ -1,15 +1,15 @@
 
-//var arr = JSON.parse(localStorage.getItem('formdata')) || [];
+var admindata = JSON.parse(localStorage.getItem('admindata')) || [];
 function dologin(){
     var email=document.loginform.email.value;
     var password=document.loginform.password.value;
     if (email==null || email==""|| password==null || password==""){  
         alert("Please fill all field"); 
     }
-    //else if(sessionStorage.getItem('admin-loggedin')){
-    //    alert("Admin Alreadt Loggedin")
-    //}
-    else if(email === "admin@123.com" && password === "admin123"){
+    else if(sessionStorage.getItem('admin-loggedin')){
+        alert("Admin already logged-in!!");
+    }
+    else if(email === admindata[0]['email'] && password === admindata[0]['password']){
         window.location.href="dashboard.html";
         sessionStorage.setItem('admin-loggedin',true);
     }
@@ -17,6 +17,6 @@ function dologin(){
         alert("Please fill correct data");
     }
 }
-function register(){
+function redirect(){
     window.location.href="register.html";
 }
---
+++ test 21-01-2021/regester.js
@@ -0,0 +1,38 @@
+if(sessionStorage.getItem('admin-loggedin')){
+    alert("Admin Already Registered!!");
+    document.getElementById("register").disabled = true; 
+}
+var admindata = [];
+function doregister(){
+    var name=document.regform.name.value;
+    var email=document.regform.email.value;
+    var password=document.regform.password.value;
+    var repassword=document.regform.repassword.value;
+    var city=document.regform.city.value;
+    var state=document.regform.state.value;
+    var terms = document.regform.condition.checked;
+    //console.log(name, email,password, repassword,city,state,terms);
+    if (name==null || name=="" || email==null || email==""|| password==null || password=="" 
+    ||repassword==null || repassword=="" || city==null || city==""|| state==null || state=="" || terms == false){  
+        alert("Please fill all field"); 
+    }
+    else if(password !== repassword){
+        alert("Password doesn't match"); 
+    }
+    else if(localStorage.getItem('admindata')){
+        alert("Admin Already Registered!!"); 
+    }
+    else {
+        var admin = {
+            name : name,
+            email : email,
+            password: password,
+            city: city,
+            state: state,
+        }
+        admindata.push(admin);
+        console.log(admindata);
+    
+        localStorage.setItem('admindata', JSON.stringify(admindata));
+    }
+}
